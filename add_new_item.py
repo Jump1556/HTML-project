2fb92c63e00371e91f3247cab00312eb280cabe6
Anastasiia Valdenmaiier <anastasiia.kostashenko@gmail.com>
2017-11-24 23:12:21 +0000

    Delete add_new_item.py

--- add_new_item.py
+++
@@ -1,8 +0,0 @@
-# Add new item to your stock
-
-
-name = input("Enter a name of medicine")
-quantity = input("Enter quantity")
-form = input("Enter a form, Tablet, Drops, Bottle for example")
-action = input("Enter uses of medicine")
-print (name, quantity, form, action)
